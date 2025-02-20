3a38f6908da1a5991da53606654162d09609bde2
ZRKhoa <gianglamdangkhoa139@gmail.com>
2025-02-20 13:05:00 +0700

    Fix bug

--- DHKPTSocial/backend_Ecommerce/index.js
+++ DHKPTSocial/backend_Ecommerce/index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app); // Tạo server HTTP
 const io = new Server(server, {
     cors: {
-        origin: "https://dkkptsocial.netlify.app", // Đảm bảo đúng cổng của frontend
+        origin: "https://dhkptsocial.netlify.app", // Đảm bảo đúng cổng của frontend
         credentials: true,
     },
 });
@@ -18,7 +18,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://dkkptsocial.netlify.app",
+        origin: "https://dhkptsocial.netlify.app",
         credentials: true,
     })
 );
