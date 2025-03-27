f6b406b1d5bcfaebec80490dabf3915fc3cafa06
gianglamdangkhoa130904 <120297899+gianglamdangkhoa130904@users.noreply.github.com>
2025-03-27 10:57:05 +0700

    Update App.jsx

--- DHKPTSocial/frontend/src/App.jsx
+++ DHKPTSocial/frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import {Routes, Route, useLocation} from 'react-router-dom'
 import React from 'react'
@@ -26,11 +28,55 @@ import SellerVoucher from './pages_e_commerce/seller/SellerVoucher.jsx'
 import SellerRevenue from './pages_e_commerce/seller/SellerRevenue.jsx'
 import SellerTax from './pages_e_commerce/seller/SellerTax.jsx'
 import SellerOrder from './pages_e_commerce/seller/SellerOrder.jsx'
+
 import ProductDetail from './pages_e_commerce/components/ProductDetail.jsx'
 import CartPage from './pages_e_commerce/components/CartPage.jsx'
 import InforPayment from './pages_e_commerce/customer/InforPayment.jsx'
 import PaymentPage from './pages_e_commerce/customer/PaymentPage.jsx'
 import ReturnPayment from './pages_e_commerce/customer/ReturnPayment.jsx'
+import OrderList from "./pages_e_commerce/customer/OrderList.jsx";
+import OrderDetail from "./pages_e_commerce/customer/OrderDetail";
+import AccountLayout from "./pages_e_commerce/customer/AccountLayout";
+import AccountPage from "./pages_e_commerce/customer/AccountPage.jsx";
+
+import AssignOrdersWarehouse from './pages_e_commerce/shipper/AssignOrdersWarehouse'
+import LoginShipper from './pages_e_commerce/shipper/LoginShipper'
+
+import LoginWarehouseStaff from './pages_e_commerce/shipper/LoginWarehouseStaff'
+import ShipperDashboard from './pages_e_commerce/shipper/ShipperDashboard'
+import ShipperOrders from './pages_e_commerce/shipper/ShipperOrders'
+import WarehouseDashboard from './pages_e_commerce/shipper/WarehouseDashboard'
+import WarehouseOrders from './pages_e_commerce/shipper/WarehouseOrders'
+import WarehouseOrdersPage from './pages_e_commerce/shipper/WarehouseOrdersPage'
+import WarehousePage from './pages_e_commerce/shipper/WarehousePage'
+import WarehouseShippers from './pages_e_commerce/shipper/WarehouseShippers';
+
+import AdminLogin from './pages_e_commerce/admin/Login'
+import AdminHome from './pages_e_commerce/admin/AdminHome'
+
+import {
+  Chart as ChartJS,
+  CategoryScale, // Đăng ký CategoryScale
+  LinearScale,   // Đăng ký LinearScale
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Đăng ký các thành phần cần thiết
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 const App = () => {
   return (
       <Routes>
@@ -60,6 +106,12 @@ const App = () => {
         <Route path='/e-commerce/customer/infor-payment' element={<InforPayment/>}/>
         <Route path='/e-commerce/customer/payment' element={<PaymentPage/>}/>
         <Route path='/e-commerce/customer/return-payment' element={<ReturnPayment/>}/>
+        <Route path="/e-commerce/customer/account" element={<AccountLayout />}>
+          <Route index element={<AccountPage />} /> {/* /account */}
+          <Route path="orders" element={<OrderList />} /> {/* /account/orders */}
+          <Route path="order/:orderId" element={<OrderDetail />} />
+        </Route>
+
         {/* Seller */}
         <Route path='/e-commerce/seller/view-store/:id' element={<ViewStore/>}/>
         <Route path='/e-commerce/seller/list-store' element={<ListStoreEcommerce/>}/>
@@ -72,9 +124,19 @@ const App = () => {
         <Route path='/e-commerce/seller/tax' element={<SellerTax/>}/>
         <Route path='/e-commerce/seller/order' element={<SellerOrder/>}/>
         {/* Shipper */}
-
+        <Route path="/shipper-login" element={<LoginShipper />} />
+        <Route path="/assignorderswarehouse" element={<AssignOrdersWarehouse/>}/>
+        <Route path="/loginwarehousestaff" element={<LoginWarehouseStaff/>}/>
+        <Route path="/shipperdashboard" element={<ShipperDashboard/>}/>
+        <Route path="/shipperorders" element={<ShipperOrders/>}/>
+        <Route path="/warehousedashboard" element={<WarehouseDashboard/>}/>
+        <Route path="/warehouseorders" element={<WarehouseOrders/>}/>
+        <Route path="/warehouseorderspage" element={<WarehouseOrdersPage/>}/>
+        <Route path="/warehousepage" element={<WarehousePage/>}/>
+        <Route path="/warehouseshippers" element={<WarehouseShippers/>}/>   
         {/* Admin */}
-
+        <Route path='/admin/e-commerce/login' element={<AdminLogin/>}/>
+        <Route path='/admin/e-commerce' element={<AdminHome/>} />
       </Routes>
   )
 }
